d5f287b1dfc9a4d30b7895fa2e8a9bedef8ed821
Michael Cittar <mcittar@gmail.com>
2017-02-02 14:35:53 -0800

    Complete execution problems

--- execution.rb
+++ execution.rb
@@ -10,7 +10,6 @@ class Array
           break if self[i] > self[j]
           return self[i]
         end
-
       end
     end
   end
@@ -18,8 +17,8 @@ class Array
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p list.my_min  # =>  -5
-# O(n^2)
+list.my_min  # =>  -5
+# O(n**2)
 
 def my_min_fast(list)
   min = 0
@@ -28,12 +27,13 @@ def my_min_fast(list)
   end
   min
 end
-p my_min_fast(list)  # =>  -5
+my_min_fast(list)  # =>  -5
 # O(n)
 
+# O(n**7)
 def sub_sum(list)
   sub_arrs = []
-  byebug
+  # byebug
   list.each_index do |i|
     list.each_index do |j|
       if j >= i
@@ -41,8 +41,42 @@ def sub_sum(list)
       end
     end
   end
-  sub_arrs
+  size = []
+  sub_arrs.each do |arr|
+    size << arr.inject(&:+)
+  end
+  size.max
 end
 
 list = [5, 3, -7]
-p sub_sum(list) # => 8
+sub_sum(list) # => 8
+
+def fast_sub_sum(list)
+
+  return list.max if list.all?(&:negative?)
+
+  best_max = 0
+  running_max = 0
+  list.each do |el|
+
+    running_max += el
+
+    if running_max < 0
+      running_max = 0
+      next
+    end
+    best_max = running_max if running_max > best_max
+  end
+
+  best_max
+end
+list = [2, 3, -6, 7, -6, 7]
+p fast_sub_sum(list) # => 8 (from [7, -6, 7])
+list = [5, 3, -7]
+p fast_sub_sum(list) # => 8
+list = [-5, -1, -3]
+p fast_sub_sum(list) # => -1 (from [-1])
+list = [1, 2, 3]
+p fast_sub_sum(list) # => 6
+list = [7, -6, 7]
+p fast_sub_sum(list) # => 8
